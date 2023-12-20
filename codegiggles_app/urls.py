33f89cc64028c43b1553fbb4dc6713bdcfa14202
jurco-exe <jurajbosy1@gmail.com>
2023-12-20 20:11:14 +0100

    feat: styles are done for alsmost everything | need to done styles for dynamic pages

--- codegiggles_app/urls.py
+++ codegiggles_app/urls.py
@@ -1,9 +1,10 @@
 # codegiggles_app/urls.py
 from django.urls import path
-from .views import home, snippets, add_snippet
+from .views import home, snippets, add_snippet, snippet_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('snippets/', snippets, name='snippets'),
     path('add_snippet/', add_snippet, name='add_snippet'),
+    path('snippets/<int:snippet_id>/', snippet_detail, name='snippet_detail'),
 ]
--- codegiggles_app/views.py
+++ codegiggles_app/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Snippet
 from .forms import SnippetForm
 
+
 def home(request):
   #! latest 8 snippets
-  snippets = Snippet.objects.all()[:8]
+  snippets = Snippet.objects.all().order_by('-created_at')[:8]
   return render(request, 'codegiggles_app/home.html', {'snippets': snippets})
  
 def snippets(request):
-  snippets = Snippet.objects.all()
+  snippets = Snippet.objects.all().order_by('-created_at')
   return render(request, 'codegiggles_app/snippets.html', {'snippets': snippets})
 
 def add_snippet(request):
@@ -22,3 +23,7 @@ def add_snippet(request):
         form = SnippetForm()
 
     return render(request, 'codegiggles_app/add_snippet.html', {'form': form})
+
+def snippet_detail(request, snippet_id):
+   snippet = get_object_or_404(Snippet, pk=snippet_id)
+   return render(request, 'codegiggles_app/snippet_detail.html', {'snippet': snippet})
